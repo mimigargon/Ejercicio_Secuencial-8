92de242c1f080aa9f52d6e5ebdbdca861d41ffca
Mimi <mireiag77@gmail>
2023-04-04 14:36:50 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_Secuencial_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num < 0)
+            {
+                Console.WriteLine("El número es negativo");
+            }else if(num > 0)
+            {
+                Console.WriteLine("El número es positivo");
+            }else if (num == 0)
+            {
+                Console.WriteLine("El número es 0");
+            }
+        }
+    }
+}
